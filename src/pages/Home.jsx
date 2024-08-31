895208242d945508272b31686109f2b6b5ffc6d1
Aaris-Kazi <aariskazi@gmail.com>
2024-09-01 01:10:23 +0530

    UPDATE: home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 export const Home = () => {
     const username = localStorage.getItem("cloud_drive_username");
-  return (
-    <div>Hi, {username}</div>
-  )
+    const token = localStorage.getItem("cloud_drive_access_token");
+    if (token != null) {
+      return (
+        <div className='whiteColor'>Hi, {username}</div>
+      );
+    } else {
+      return (
+        <div className='whiteColor'>Please login</div>
+      );
+
+    }
 }
